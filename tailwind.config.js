8fe3bbc7d077263e2920cd9c00b3c43c8b4bd254
alessandrodiazrodrigues <alessandrorodrigues@cvc.com.br>
2025-09-07 18:46:18 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'rotary-blue': '#17458f',
+        'rotary-gold': '#f7a81b',
+      },
+    },
+  },
+  plugins: [],
+}
